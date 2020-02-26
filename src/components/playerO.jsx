897276d18af054814a27fc1c62a6c75b4f89569d
Student <example@example.com>
2020-02-26 11:45:29 -0800

    tictactoe finished part one

---
+++ src/components/playerO.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PlayerO extends React.Component{
+  super(props){
+    win count = {},
+    playerName = props.string,
+
+
+
+  }
+  render(){
+    return(
+
+    )
+  }
+}
+
+export default PlayerO;
---
+++ src/components/playerX.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PlayerX extends React.Component{
+  super(props){
+    win count = {},
+    playerName = props.string,
+    
+
+
+  }
+  render(){
+    return(
+
+    )
+  }
+}
+
+export default PlayerX;
